c51af9065ed72a8d43110fa0ad7da7d512ad28d9
Autoonus <142937531+Autoonus@users.noreply.github.com>
2023-10-25 19:10:11 -0400

    fixed file navigation of LoginNavbar

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import LoginNavbar from '../components/LoginNavbar';
+import LoginNavbar from '../components/LoginNavbar/loginNavbar';
 import Login from '../components/Login';
 import Register from '../components/Register';
 
