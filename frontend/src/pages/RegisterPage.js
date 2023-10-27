c1543700d5c0d48d3f08e6adddfb0f0fce3fcddc
peytonmlr <75341088+peytonmlr@users.noreply.github.com>
2023-10-27 17:11:43 -0400

    Add files via upload

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Register from '../components/Register';
+import LoginNavbar from '../components/LoginNavbar/LoginNavbar';
+
+const RegisterPage = () =>
+{
+
+    return(
+      <div>
+        <LoginNavbar />
+        <Register />
+      </div>
+    );
+};
+
+export default RegisterPage;
