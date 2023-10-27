7bc1fbd09f4795c5c85d100fc8c816752e68446a
peytonmlr <75341088+peytonmlr@users.noreply.github.com>
2023-10-27 17:14:08 -0400

    Added navigation for login and register

--- frontend/src/components/LoginNavbar/LoginNavbar.js
+++ frontend/src/components/LoginNavbar/LoginNavbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 import './LoginNavbarStyles.css'
+import {useNavigate} from 'react-router-dom';
 
 function LoginNavbar()
 {
+   const navigate = useNavigate();
+
+   const navigateLogin = () => {
+       navigate('/');
+   }
+   const navigateRegister = () => {
+       navigate('/register');
+   }
    return(
       <div className='navbar'>
          <div className="logo">
@@ -13,8 +22,8 @@ function LoginNavbar()
             <li>Home</li>
             <li>About</li>
             <li>GitHub</li>
-            <li>Login</li>
-            <li>Sign-Up</li>
+            <li onClick={navigateLogin}>Login </li>
+            <li onClick={navigateRegister}>Sign-Up </li>
          </ul>
       </div>
    );
