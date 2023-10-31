816bc2998c04f1e23e52b3a386998dbcd2530a7a
Autoonus <142937531+Autoonus@users.noreply.github.com>
2023-10-31 10:35:33 -0400

    Trying Nav Icons

--- frontend/src/components/LoginNavbar/LoginNavbar.js
+++ frontend/src/components/LoginNavbar/LoginNavbar.js
@@ -14,13 +14,16 @@ function LoginNavbar()
        navigate('/register');
    }
    return(
-      <div className="nav">
+    <div className="nav">
       <div className="nav-text">
         <div className="my-steam-list">Mysteamlist</div>
         <div className="text-wrapper">about</div>
         <div className="div">browse</div>
         <div className="text-wrapper-2" onClick={navigateRegister}>sign up</div>
         <div className="text-wrapper-3" onClick={navigateLogin}>login</div>
+        <div className="nav-icons">
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path></svg>
+        </div>
       </div>
     </div>
       
