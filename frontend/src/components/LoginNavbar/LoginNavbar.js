aa214becbf9f1228d58f79a0f2ea7ecedbf8cadc
Autoonus <142937531+Autoonus@users.noreply.github.com>
2023-10-31 17:22:53 -0400

    navbar-bg changes

--- frontend/src/components/LoginNavbar/LoginNavbar.js
+++ frontend/src/components/LoginNavbar/LoginNavbar.js
@@ -18,7 +18,7 @@ function LoginNavbar()
    }
 
    return(
-    <div className='navbar'>
+    <div className={nav ? 'navbar navbar-bg' : 'navbar'}>
       <div className="title">
         <h2>Mysteamlist</h2>
       </div>
@@ -29,7 +29,7 @@ function LoginNavbar()
         <li onClick={navigateLogin}>Login</li>
       </ul>
       <div className="nav-icons">
-        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+        <svg style={{marginRight: '1rem'}} stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002.002zM8 7a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"></path></svg>
       </div>
       <div className="hamburger" onClick={handleNav}>
