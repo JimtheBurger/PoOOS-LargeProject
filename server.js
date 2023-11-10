116827326573fc91b62133e0226fa2a719c47cc6
John Luong <johnathanluong7@gmail.com>
2023-11-10 15:05:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -326,37 +326,18 @@ app.post("/api/searchGamesIGDB", async (req, res, next) => {
   const { name, CLIENT_ID, ACCESS_TOKEN } = req.body;
   const api_url = `https://api.igdb.com/v4/games`;
 
-  /*
   fetch(
     api_url,
     { method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Client-ID': CLIENT_ID,
-        'Authorization': 'Bearer ' + ACCESS_TOKEN,
+        'Client-ID': `${CLIENT_ID}`,
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
       },
-      body: `fields name;`
+      body: `fields name; search "${name}"`
   })
     .then(response => {
       res.status(200).json(response.json());
-    })
-    .catch(err => {
-        console.error(err);
-    });
-  */
-
-  fetch(
-    "https://api.igdb.com/v4/games",
-    { method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Client-ID': 'Client ID',
-        'Authorization': 'Bearer access_token',
-      },
-      body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-  })
-    .then(response => {
-        console.log(response.json());
     })
     .catch(err => {
         console.error(err);
@@ -375,8 +356,8 @@ app.post("/api/searchSteamID", async (req, res, next) => {
     { method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Client-ID': CLIENT_ID,
-        'Authorization': 'Bearer ' + ACCESS_TOKEN,
+        'Client-ID': `${CLIENT_ID}`,
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
       }, 
       body: `fields game, url; where game = ${IGDB_ID} & category = 13;`
   })
