e2b997c2b2a150469375def6eba63d1eceef5973
peytonmlr <75341088+peytonmlr@users.noreply.github.com>
2023-11-10 19:57:53 -0500

    Add files via upload

---
+++ frontend/src/pages/WelcomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TopNav from "../components/TopNav/TopNav";
+import Welcome from "../components/Forms/Welcome";
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <TopNav />
+      <Welcome />
+    </div>
+  );
+};
+
+export default WelcomePage;
