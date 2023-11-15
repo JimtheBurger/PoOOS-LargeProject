d22d5dd2e0e95169ba90be91f93e12f85a6df1f5
peytonmlr <peytonmiller89@gmail.com>
2023-11-15 16:27:37 -0500

    GamePage loads from AppID in url

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useParams } from "react-router-dom";
 
 function Game() {
   const { AppID } = useParams();
   const [games, setGames] = useState([]);
-  const [game, setGame] = useState([]);
+  const [SelectedGame, setSelectedGame] = useState({});
+  const [loading, setLoading] = useState(true);
 
   //Gets game data from database
   useEffect(() => {
     const fetchData = async () => {
       try {
         let response;
-        if (process.env.NODE_ENV === 'production') {
-          response = await fetch('https://mysteamlist.com/api/games');
+        if (process.env.NODE_ENV === "production") {
+          response = await fetch("https://mysteamlist.com/api/games");
         } else {
-          response = await fetch('http://localhost:5000/api/games');
+          response = await fetch("http://localhost:5000/api/games");
         }
 
         if (!response.ok) {
@@ -28,26 +29,47 @@ function Game() {
 
         const data = await response.json();
         setGames(data);
-        const foundGame = data.find((game) => game.AppID === AppID);
-        if (foundGame) {
-          setGame(foundGame); // Set the found game
-        }
+        setLoading(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, [AppID]);
+  }, []);
+
+  useEffect(() => {
+    console.log("AppID:", AppID);
+    console.log("Games:", games);
+    // Check if games are available and AppID is valid before finding the game
+    if (games.length > 0 && AppID) {
+      const foundGame = games.find((game) => game.AppID === Number(AppID));
+      console.log("Found Game:", foundGame);
+      if (foundGame) {
+        setSelectedGame(foundGame);
+      }
+    }
+  }, [games, AppID]);
+
+  if (loading) {
+    return <div>Loading...</div>; // Show a loading indicator while fetching data
+  }
 
   return (
     <Container>
       <Col md={6}>
         <Card className="text-center">
-          <Card.Img variant="top" src={game.Image} alt={game.Name} />
+          {SelectedGame.Image && (
+            <Card.Img
+              variant="top"
+              src={SelectedGame.Image}
+              alt={SelectedGame.Name}
+            />
+          )}
           <Card.Body>
-            <h5>{game.Name}</h5>
-            <p>{game.Description}</p>
+            {SelectedGame.Name && <h5>{SelectedGame.Name}</h5>}
+            {SelectedGame.Description && <p>{SelectedGame.Description}</p>}
             <Button variant="primary">
               Add to List <i className="bi bi-caret-down-fill"></i>
             </Button>
