259c61c0b497ab9da3d6ebc0c1176bb796332eb9
peytonmlr <peytonmiller89@gmail.com>
2023-11-15 12:49:36 -0500

    added gamepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,9 @@ import ProfilePage from "./pages/ProfilePage";
 import AdminAddGames from "./pages/AdminAddGames";
 import BrowsePage from "./pages/BrowsePage";
 import AppContext from "./context/AppContext";
+import GamePage from "./pages/GamePage";
+import ListPage from "./pages/ListPage";
+import BrowseDBPage from "./pages/BrowseDBPage";
 
 function App() {
   const [user, setUser] = useState({ Username: "", IsLoggedIn: false });
@@ -33,9 +36,12 @@ function App() {
           <Route path="/verify-email" element={<VerifyEmailPage />} />
           <Route path="/resetPassword" element={<ResetPasswordPage />} />
           <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
-          <Route path="/browse" element={<BrowsePage />} />
+          <Route path="/browse" element={<BrowseDBPage />} />
+          <Route path="/browse2" element={<BrowsePage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/adminAddGames" element={<AdminAddGames />} />
+          <Route path="/game/:AppID" element={<GamePage />} />
+          <Route path="/list" element={<ListPage />} />
         </Routes>
       </BrowserRouter>
     </AppContext.Provider>
