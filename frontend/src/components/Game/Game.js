ebb361504d9321fb6eb8b1517e9218e7189deca8
peytonmlr <peytonmiller89@gmail.com>
2023-11-15 21:07:53 -0500

    improvements & additions

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useParams } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs";
 
 function Game() {
   const { AppID } = useParams();
@@ -40,12 +41,9 @@ function Game() {
   }, []);
 
   useEffect(() => {
-    console.log("AppID:", AppID);
-    console.log("Games:", games);
     // Check if games are available and AppID is valid before finding the game
     if (games.length > 0 && AppID) {
       const foundGame = games.find((game) => game.AppID === Number(AppID));
-      console.log("Found Game:", foundGame);
       if (foundGame) {
         setSelectedGame(foundGame);
       }
@@ -58,24 +56,55 @@ function Game() {
 
   return (
     <Container>
-      <Col md={6}>
-        <Card className="text-center">
-          {SelectedGame.Image && (
-            <Card.Img
-              variant="top"
-              src={SelectedGame.Image}
-              alt={SelectedGame.Name}
-            />
-          )}
-          <Card.Body>
-            {SelectedGame.Name && <h5>{SelectedGame.Name}</h5>}
-            {SelectedGame.Description && <p>{SelectedGame.Description}</p>}
-            <Button variant="primary">
-              Add to List <i className="bi bi-caret-down-fill"></i>
-            </Button>
-          </Card.Body>
-        </Card>
-      </Col>
+      <Card>
+        <Card.Body>
+          <Row>
+            <Col xs={12} md={4}>
+              <Card.Img src={SelectedGame.Image} />
+            </Col>
+            <Col xs={12} md={8} className="d-flex align-items-center">
+              <div>
+                <h3>{SelectedGame.Name}</h3>
+                <p>{SelectedGame.Description}</p>
+              </div>
+            </Col>
+          </Row>
+          <Button
+            style={{ marginTop: "10px", marginRight: "5px" }}
+            variant="purple"
+          >
+            Add to List <i className="bi bi-caret-down-fill"></i>
+          </Button>
+          <Button style={{ marginTop: "10px" }} variant="warning">
+            <BsStarFill size={20} /> {/* Star-shaped button */}
+          </Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ height: "100%" }}>
+        <Card.Body>
+          <p className="mb-2">Title: {SelectedGame.Name} </p>
+          <p className="mb-2">
+            Genres: {SelectedGame.Genres && SelectedGame.Genres.join(", ")}{" "}
+          </p>
+          <p className="mb-2">
+            Developers:{" "}
+            {SelectedGame.Developers && SelectedGame.Developers.join(", ")}{" "}
+          </p>
+          <p className="mb2">
+            Publishers:{" "}
+            {SelectedGame.Publishers && SelectedGame.Publishers.join(", ")}{" "}
+          </p>
+          <p className="mb-2">
+            Price:{" "}
+            {SelectedGame.Price ? SelectedGame.Price.final_formatted : "N/A"}{" "}
+          </p>
+          <p className="mb-0">
+            Release Date:{" "}
+            {SelectedGame.Release ? SelectedGame.Release.date : "N/A"}{" "}
+          </p>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
