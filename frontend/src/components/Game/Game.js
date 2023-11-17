496dc2a06e55b58c66385e8595e3c29f184a8888
peytonmlr <peytonmiller89@gmail.com>
2023-11-17 14:42:29 -0500

    background & text changes

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -55,56 +55,71 @@ function Game() {
   }
 
   return (
-    <Container>
-      <Card>
-        <Card.Body>
-          <Row>
-            <Col xs={12} md={4}>
-              <Card.Img src={SelectedGame.Image} />
-            </Col>
-            <Col xs={12} md={8} className="d-flex align-items-center">
-              <div>
-                <h3>{SelectedGame.Name}</h3>
-                <p>{SelectedGame.Description}</p>
-              </div>
-            </Col>
-          </Row>
-          <Button
-            style={{ marginTop: "10px", marginRight: "5px" }}
-            variant="purple">
-            Add to List <i className="bi bi-caret-down-fill"></i>
-          </Button>
-          <Button style={{ marginTop: "10px" }} variant="warning">
-            <BsStarFill size={20} /> {/* Star-shaped button */}
-          </Button>
-        </Card.Body>
-      </Card>
+    <div
+      className="game-content"
+      style={{
+        backgroundColor: "#d0cdd6",
+        minHeight: "100vh",
+        padding: "20px",
+        position: "relative",
+      }}
+    >
+      <Container>
+        <Card style={{ boxShadow: "0 4px 8px rgba(0,0,0,0.1)" }}>
+          <Card.Body>
+            <Row>
+              <Col xs={12} md={4}>
+                <Card.Img src={SelectedGame.Image} />
+              </Col>
+              <Col xs={12} md={6} className="d-flex align-items-center">
+                <div style={{ marginTop: "10px" }}>
+                  <h3>{SelectedGame.Name}</h3>
+                  <p>{SelectedGame.Description}</p>
+                </div>
+              </Col>
+            </Row>
+            <Button
+              style={{ marginTop: "10px", marginRight: "5px" }}
+              variant="purple"
+            >
+              Add to List <i className="bi bi-caret-down-fill"></i>
+            </Button>
+            <Button style={{ marginTop: "10px" }} variant="purple">
+              <BsStarFill size={20} style={{ color: "#FFD700" }} />{" "}
+              {/* Star-shaped button */}
+            </Button>
+          </Card.Body>
+        </Card>
 
-      <Card style={{ height: "100%" }}>
-        <Card.Body>
-          <p className="mb-2">Title: {SelectedGame.Name} </p>
-          <p className="mb-2">
-            Genres: {SelectedGame.Genres && SelectedGame.Genres.join(", ")}{" "}
-          </p>
-          <p className="mb-2">
-            Developers:{" "}
-            {SelectedGame.Developers && SelectedGame.Developers.join(", ")}{" "}
-          </p>
-          <p className="mb2">
-            Publishers:{" "}
-            {SelectedGame.Publishers && SelectedGame.Publishers.join(", ")}{" "}
-          </p>
-          <p className="mb-2">
-            Price:{" "}
-            {SelectedGame.Price ? SelectedGame.Price.final_formatted : "N/A"}{" "}
-          </p>
-          <p className="mb-0">
-            Release Date:{" "}
-            {SelectedGame.Release ? SelectedGame.Release.date : "N/A"}{" "}
-          </p>
-        </Card.Body>
-      </Card>
-    </Container>
+        <Card style={{ marginTop: "10px" }}>
+          <Card.Body>
+            <p className="mb-2">
+              <strong>Title:</strong> {SelectedGame.Name}{" "}
+            </p>
+            <p className="mb-2">
+              <strong>Genres:</strong>{" "}
+              {SelectedGame.Genres && SelectedGame.Genres.join(", ")}{" "}
+            </p>
+            <p className="mb-2">
+              <strong>Developers:</strong>{" "}
+              {SelectedGame.Developers && SelectedGame.Developers.join(", ")}{" "}
+            </p>
+            <p className="mb-2">
+              <strong>Publishers:</strong>{" "}
+              {SelectedGame.Publishers && SelectedGame.Publishers.join(", ")}{" "}
+            </p>
+            <p className="mb-2">
+              <strong>Publishers:</strong>{" "}
+              {SelectedGame.Price ? SelectedGame.Price.final_formatted : "N/A"}{" "}
+            </p>
+            <p className="mb-0">
+              <strong>Release Date:</strong>{" "}
+              {SelectedGame.Release ? SelectedGame.Release.date : "N/A"}{" "}
+            </p>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
   );
 }
 export default Game;
